43e433ac0d6082ea069f3946d32856a29cd13e83
frenklinwong <frenklinfarrelwong@gmail.com>
2026-02-26 13:50:45 +0800

    web.js

---
+++ web.js
@@ -0,0 +1,25 @@
+/*
+
+Home page:
+1. Edit profile (link buat ke edit profile) (tombol Home page)
+1.1 Profile
+1.2 Course
+
+2. Categories (Dijabarin)
+IT Economy History
+2.2 IT - A (Preview berbentuk artikel + Add to my course) (tombol Home page sama Edit profile)
+2.2 IT - B
+
+2.2 Economy - A
+2.2 Economy - B
+
+Router
+
+('/') > Login/Register/Log out ke website (rizky)
+('/Homepage') (frenklin)
+('/:id/Edit') >> buat ke edit profile (user) (frenklin)
+('/:id/Add') >> nambahin courses ke category (admin) (rizky)
+('/:id/Delete') >> hapus courses dari category (admin) (rizky)
+('/:id/Courses') >> buat lihat courses yang dipunya (user) (frenklin)
+
+('/Categories/Courses') >> buat ngelihat course apa aja + nambah course ke ('/UserId/Courses') (berdua)
